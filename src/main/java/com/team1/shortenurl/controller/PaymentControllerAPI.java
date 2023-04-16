15945e818cd6da7f2377556ef2f6ffdb2ef13e88
Zitong Li <zitongliapp@gmail.com>
2023-04-16 15:47:03 -0500

    test pay2.0

--- src/main/java/com/team1/shortenurl/controller/PaymentControllerAPI.java
+++ src/main/java/com/team1/shortenurl/controller/PaymentControllerAPI.java
@@ -4,16 +4,22 @@ import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
 import com.team1.shortenurl.entity.CustomerData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class PaymentControllerAPI {
+
+    @Value("$(stripe.apikey)")
+    String stripeKey;
     @RequestMapping("/testPay")
     public CustomerData test(@RequestBody CustomerData data) throws StripeException {
-        Stripe.apiKey = "sk_test_51MxbpCHbnp0twtPHd8b8U1v5B581rb64whdE2XQVB1vdNXjNigUGNmyKEDyYPw8cIw9zzivQrq1VpoHjaCs8NF5T00R28MrpaR";
+        Stripe.apiKey = stripeKey;
 
         Map<String, Object> params = new HashMap<>();
         params.put("name", data.getName());
