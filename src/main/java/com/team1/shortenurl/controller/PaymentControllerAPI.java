58714b3bcaa7733e01fef4152497576669823748
teo <mcgeelin7@gmail.com>
2023-04-16 16:30:49 -0500

    Paytest

--- src/main/java/com/team1/shortenurl/controller/PaymentControllerAPI.java
+++ src/main/java/com/team1/shortenurl/controller/PaymentControllerAPI.java
@@ -1,5 +1,6 @@
 package com.team1.shortenurl.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
@@ -14,7 +15,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-
 
 @RestController
 public class PaymentControllerAPI {
@@ -33,10 +33,15 @@ public class PaymentControllerAPI {
         return data;
     }
 
+    @RequestMapping("/testPayPage")
+    public void testPage(@RequestBody String json){
+        System.out.println("success");
+    }
+
     @RequestMapping("/pay")
     @ResponseBody
-    public Map<String,String>  pay(HttpServletRequest httpRequest, HttpServletResponse httpResponse){
-        Map<String,String> resultMap = new HashMap<>();
+    public String pay(HttpServletRequest httpRequest, HttpServletResponse httpResponse){
+        JSONObject res = new JSONObject();
         try {
             Stripe.apiKey = "sk_test_51MxbpCHbnp0twtPHd8b8U1v5B581rb64whdE2XQVB1vdNXjNigUGNmyKEDyYPw8cIw9zzivQrq1VpoHjaCs8NF5T00R28MrpaR";
             Map<String, Object> params = new HashMap<String, Object>();
@@ -45,9 +50,9 @@ public class PaymentControllerAPI {
             params.put("payment_method_types", paymentMethodTypes);
             ArrayList<HashMap<String, Object>> lineItems = new ArrayList<>();
             HashMap<String, Object> lineItem = new HashMap<String, Object>();
-            lineItem.put("name", "胡鹏飞测试商品");
-            lineItem.put("description", "这是一个测试单描述");
-            lineItem.put("amount", 500); // 支付金额
+            lineItem.put("name", "sslt1premium");
+            lineItem.put("description", "This is a test");
+            lineItem.put("amount", 100); // 支付金额
             lineItem.put("currency", "usd"); //支付币种
             lineItem.put("quantity", 1);
             lineItems.add(lineItem);
@@ -55,16 +60,19 @@ public class PaymentControllerAPI {
             //TODO 必须使用https 返回的回调地址
             String uuid = UUID.randomUUID().toString();
             params.put("client_reference_id", uuid);//业务系统唯一标识 即订单唯一编号
-            log.info("uuid:{}");
-            params.put("success_url", URLUtils.getBaseURl(httpRequest)+"/paySuccess");// 支付成功跳转页面
-            params.put("cancel_url",  URLUtils.getBaseURl(httpRequest)+"/payError");// 支付失败跳转页面
+            //log.info("uuid:{}");
+            res.put("uuid", uuid);
+            //params.put("success_url", URLUtils.getBaseURl(httpRequest)+"/paySuccess");// 支付成功跳转页面
+            //params.put("cancel_url",  URLUtils.getBaseURl(httpRequest)+"/payError");// 支付失败跳转页面
+            params.put("success_url", "https://sslt1.herokuapp.com/testPayPage");// 支付成功跳转页面
+            params.put("cancel_url",  "https://sslt1.herokuapp.com/testPayPage");// 支付失败跳转页面
             Session session = Session.create(params);
             String sessionId = session.getId();
-            log.info("sessionId :{}",session.getId());
-            resultMap.put("sessionId",sessionId);
+            //log.info("sessionId :{}",session.getId());
+            res.put("sessionId", sessionId);
         } catch (StripeException e) {
             e.printStackTrace();
         }
-        return resultMap;
+        return res.toJSONString();
     }
 }
