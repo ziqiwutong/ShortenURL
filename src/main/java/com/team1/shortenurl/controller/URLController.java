3f748f5008a1f93b8c24f4dc27ad186b90981cfe
teo <mcgeelin7@gmail.com>
2023-04-16 17:18:20 -0500

    Batch

--- src/main/java/com/team1/shortenurl/controller/URLController.java
+++ src/main/java/com/team1/shortenurl/controller/URLController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 // @RequestMapping(value = "/urlProcess")
@@ -71,5 +73,34 @@ public class URLController {
         resolveUrlService.updateCount(url.getCountClick() + 1, url.getServiceId());
         String longUrl = url.getLongUrl();
         response.sendRedirect(longUrl);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/diyShortenUrl")
+    public String diyShortenUrl(@RequestBody String json) {
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        int uid = jsonObject.getIntValue("uid");
+        String longUrl = jsonObject.getString("longUrl");
+        String shortUrl = jsonObject.getString("shortUrl");
+        shortUrl = "sslt1.herokuapp.com/" + shortUrl;
+
+        Url url = shortenUrlService.checkShortUrl(shortUrl);
+        JSONObject res = new JSONObject();
+        if(Objects.isNull(url)){
+            shortenUrlService.createShortenUrl(uid, longUrl, shortUrl);
+            res.put("status", "Success");
+        }else{
+            res.put("status", "Failure");
+        }
+        return res.toJSONString();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "batchShorten")
+    public String BatchUrlShorten(@RequestBody List<Url> urls){
+        for(Url url: urls){
+            System.out.println(url.toString());
+        }
+        return "nb";
     }
 }
